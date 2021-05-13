eac6b2162c10e5f8d15b61c14b9f686f5ccebfbb
Rodrigo Campos <rod.apd@gmail.com>
2021-05-12 22:46:32 -0300

    fix(sacrificer): recycle instead of suiciding

--- src/roles/sacrificer.ts
+++ src/roles/sacrificer.ts
@@ -1,3 +1,9 @@
 export default function sacrificer(creep: Creep) {
-  creep.suicide();
+  const spawn = creep.pos
+    .findInRange(FIND_MY_SPAWNS, 1, {
+      filter: (s) => !s.spawning,
+    })
+    .find(Boolean);
+  if (!spawn) return;
+  spawn.recycleCreep(creep);
 }
